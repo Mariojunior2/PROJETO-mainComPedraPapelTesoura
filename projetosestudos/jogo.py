49bce4424a23564d6299e48abb788c866b6c3fa4
Mariojunior2 <mario.junior2@alunos.sc.senac.br>
2024-09-17 09:13:01 -0300

    ABACATE

--- projetosestudos/jogo.py
+++ projetosestudos/jogo.py
@@ -9,25 +9,32 @@ jogador = input("Fale Pedra,Papel ou tesoura: ").lower()
 
 
 if jogador == computer:
-    print("Computador Escolheu: " + computer)
+    print("Você Escolheu: \n" + jogador.capitalize())
+    print("Computador Escolheu: \n" + computer)
     print("Empatou")
 elif jogador == "pedra" and computer == "papel":
-    print("Computador Escolheu: " + computer)
+    print("Você Escolheu: \n" + jogador.capitalize())
+    print("Computador Escolheu: \n" + computer.capitalize())
     print("Maquina Te Solou Gostoso")
 elif jogador == "papel" and computer == "tesoura":
-    print("Computador Escolheu: " + computer)
+    print("Você Escolheu: \n" + jogador.capitalize())
+    print("Computador Escolheu: \n" + computer.capitalize())
     print("Maquina Te Solou Gostoso")
 elif jogador == "tesoura" and computer == "pedra":
-    print("Computador Escolheu: " + computer)
-    print("Maquina Te Solou Gostoso")
+    print("Você Escolheu: \n" + jogador.capitalize())
+    print("Computador Escolheu: \n" + computer.capitalize())
+    print("Maquina Te Solou Gostoso \n")
 elif jogador == "papel" and computer == "pedra":
-    print("Computador Escolheu: " + computer)
+    print("Você Escolheu: \n" + jogador.capitalize())
+    print("Computador Escolheu: \n" + computer.capitalize())
     print("Você Solou a maquina GOSTOSO")
 elif jogador == "tesoura" and computer == "papel":
-     print("Computador Escolheu: " + computer)
+     print("Você Escolheu: \n" + jogador.capitalize())
+     print("Computador Escolheu: \n" + computer.capitalize())
      print("Você Solou a maquina GOSTOSO")
 elif jogador == "pedra" and computer == "tesoura":
-     print("Computador Escolheu: " + computer)
+     print("Você Escolheu: \n" + jogador.capitalize())
+     print("Computador Escolheu: \n" + computer.capitalize())
      print("Você Solou a maquina GOSTOSO")
 else:
-    print("Inseria um valor válido")
+    print("Inseria um valor válido \n")
