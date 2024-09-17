cdcb090d0da2e07ed5f80ecad88da5c41a39df50
Mariojunior2 <mario.junior2@alunos.sc.senac.br>
2024-09-17 09:36:18 -0300

    a

--- projetosestudos/jogo.py
+++ projetosestudos/jogo.py
@@ -13,28 +13,28 @@ if jogador == computer:
     print("Computador Escolheu: \n" + computer.capitalize()) # CAPITALIZE DEIXA A PRIMEIRA LETRA MAINSCULA
     print("Empatou")
 elif jogador == "pedra" and computer == "papel": # AND serve para espécificar mais uma condição
-    print("Você Escolheu: \n" + jogador.capitalize())
-    print("Computador Escolheu: \n" + computer.capitalize())
+    print("Você Escolheu: \n" + jogador.capitalize()) # CAPITALIZE DEIXA A PRIMEIRA LETRA MAINSCULA
+    print("Computador Escolheu: \n" + computer.capitalize()) # CAPITALIZE DEIXA A PRIMEIRA LETRA MAINSCULA
     print("Maquina Te Solou Gostoso")
-elif jogador == "papel" and computer == "tesoura":
-    print("Você Escolheu: \n" + jogador.capitalize())
-    print("Computador Escolheu: \n" + computer.capitalize())
+elif jogador == "papel" and computer == "tesoura": # AND serve para espécificar mais uma condição
+    print("Você Escolheu: \n" + jogador.capitalize()) # CAPITALIZE DEIXA A PRIMEIRA LETRA MAINSCULA
+    print("Computador Escolheu: \n" + computer.capitalize()) # CAPITALIZE DEIXA A PRIMEIRA LETRA MAINSCULA
     print("Maquina Te Solou Gostoso")
-elif jogador == "tesoura" and computer == "pedra":
-    print("Você Escolheu: \n" + jogador.capitalize())
-    print("Computador Escolheu: \n" + computer.capitalize())
+elif jogador == "tesoura" and computer == "pedra": # AND serve para espécificar mais uma condição
+    print("Você Escolheu: \n" + jogador.capitalize()) # CAPITALIZE DEIXA A PRIMEIRA LETRA MAINSCULA
+    print("Computador Escolheu: \n" + computer.capitalize()) # CAPITALIZE DEIXA A PRIMEIRA LETRA MAINSCULA
     print("Maquina Te Solou Gostoso \n")
-elif jogador == "papel" and computer == "pedra":
-    print("Você Escolheu: \n" + jogador.capitalize())
-    print("Computador Escolheu: \n" + computer.capitalize())
+elif jogador == "papel" and computer == "pedra": # AND serve para espécificar mais uma condição
+    print("Você Escolheu: \n" + jogador.capitalize()) # CAPITALIZE DEIXA A PRIMEIRA LETRA MAINSCULA
+    print("Computador Escolheu: \n" + computer.capitalize()) # CAPITALIZE DEIXA A PRIMEIRA LETRA MAINSCULA
     print("Você Solou a maquina GOSTOSO")
-elif jogador == "tesoura" and computer == "papel":
-     print("Você Escolheu: \n" + jogador.capitalize())
-     print("Computador Escolheu: \n" + computer.capitalize())
+elif jogador == "tesoura" and computer == "papel": # AND serve para espécificar mais uma condição
+     print("Você Escolheu: \n" + jogador.capitalize()) # CAPITALIZE DEIXA A PRIMEIRA LETRA MAINSCULA
+     print("Computador Escolheu: \n" + computer.capitalize()) # CAPITALIZE DEIXA A PRIMEIRA LETRA MAINSCULA
      print("Você Solou a maquina GOSTOSO")
-elif jogador == "pedra" and computer == "tesoura":
-     print("Você Escolheu: \n" + jogador.capitalize())
-     print("Computador Escolheu: \n" + computer.capitalize())
+elif jogador == "pedra" and computer == "tesoura": # AND serve para espécificar mais uma condição
+     print("Você Escolheu: \n" + jogador.capitalize()) # CAPITALIZE DEIXA A PRIMEIRA LETRA MAINSCULA
+     print("Computador Escolheu: \n" + computer.capitalize()) # CAPITALIZE DEIXA A PRIMEIRA LETRA MAINSCULA
      print("Você Solou a maquina GOSTOSO")
 else:
     print("Inseria um valor válido \n")
